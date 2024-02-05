e17c4e177c821218a5a193a484d759e0837069cc
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2024-02-05 23:37:50 +0630

    add quotes

--- hello.sh
+++ hello.sh
@@ -5,3 +5,14 @@
 GREET="Hello, Ruby."
 
 echo $GREET
+
+quotes=(
+    "The only way to do great work is to love what you do - Steve Jobs"
+    "Believe what you can and you're halfway there - Theodore Roosevelt "
+    "The way to get started is to quit talking and begin doing - Walt Disney" 
+)
+
+# get a random index of quotes
+randomIndex=$((RANDOM % ${#quotes[@]}))
+
+echo "${quotes[$randomIndex]}"
---
+++ string.rb
@@ -0,0 +1,3 @@
+
+puts string.sub(/[aeiou]/, '.') # print: S.me String
+# puts STRING
