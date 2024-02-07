329057e004708f7cc787143464d6ce0194aa71ea
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2024-02-07 20:35:22 +0630

    fix typo in shell script

--- hello.sh
+++ hello.sh
@@ -6,7 +6,7 @@ echo $GREET
 
 quotes=(
     "The only way to do great work is to love what you do - Steve Job"
-    "The way to get started is ot quit talking and begin doing - Walt Disney"
+    "The way to get started is to quit talking and begin doing - Walt Disney"
     "Believe what you can and you're halfway there - Theodore Roosevelt"
     "Doubt kils more dreams than failure ever will - Suzy Kassem"
 )
