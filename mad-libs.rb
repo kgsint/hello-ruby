9e2972e89cd4ffca7f0379528e43363195406804
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2024-02-07 21:16:39 +0630

    add mad-libs game

---
+++ mad-libs.rb
@@ -0,0 +1,14 @@
+puts "Enter a color: "
+color = gets().chomp.downcase
+
+puts "Enter a plural noun: "
+plural_noun = gets().chomp.capitalize!
+
+puts "Enter a name of celebrity"
+celebrity = gets().chomp.capitalize!
+
+puts "\n"
+# puts "\n"
+puts "Roses are #{color}"
+puts "#{plural_noun} are blue"
+puts "I love #{celebrity}"
